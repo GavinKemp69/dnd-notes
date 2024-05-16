7d70284275b6caa879cf24596f81613a049e3a14
Bobby Gribbon <bobbygribbon@gmail.com>
2024-05-15 23:35:15 -0500

    Updating script

--- scripts/update_links.py
+++ scripts/update_links.py
@@ -4,26 +4,30 @@ import re
 def update_links_in_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
+        print("read file")
 
-    updated_content = re.sub(r'\[(.*?)\]\((.*?\.md)\)', lambda m: f"[{m.group(1)}]({m.group(2)[:-3]}/)", content)
-
-    if updated_content != content:
-        with open(file_path, 'w') as file:
-            file.write(updated_content)
-        print(f"Updated links in file: {file_path}")  # Log statement
-        return True
+        updated_content = re.sub(r'\[(.*?)\]\((.*?\.md)\)', lambda m: f"[{m.group(1)}]({m.group(2)[:-3]}/)", content)
+        print("Updated content")
+        if updated_content != content:
+            print("Actual change happened")
+            with open(file_path, 'w') as file:
+                file.write(updated_content)
+            print(f"Updated links in file: {file_path}")  # Log statement
+            return True
+        else:
+            print("But no change happened!")
     return False
+        
 
 def walk_through_files():
     changed = False
     for root, _, files in os.walk('..'):
-        print(files)
         for file in files:
-            print(file)
             if file.endswith('.md'):
                 print("IS GOOD")
                 file_path = os.path.join(root, file)
                 if update_links_in_file(file_path):
+                    print("IS THREE GOOD")
                     changed = True
     return changed
 
